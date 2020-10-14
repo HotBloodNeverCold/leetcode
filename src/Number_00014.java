b5cdc8bf3c1a9086fce3dd0cd6975a2d362239da
zzh <2805669387@qq.com>
2020-10-14 13:18:57 +0800

    leetcode

---
+++ src/Number_00014.java
@@ -0,0 +1,11 @@
+/**
+ * Created by zhangzehua on 2020/10/14.
+ */
+public class Number_00014 {
+    public static void main(String[] args) {
+
+    }
+    public String longestCommonPrefix(String[] strs) {
+        return "-1";
+    }
+}
